bc78c3f2e22750b7bfe3bd2e1e8f1479544806e1
meomeocut28 <127227724+meomeocut28@users.noreply.github.com>
2023-03-07 23:07:39 +0700

    Add files via upload
    helloo

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Nguyen Anh';
+const giftUrl = 'https://www.facebook.com/nguyen.anhh.cutephomaique';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Happy 8-3';
+const contentCard = 'Chúc em luôn tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Hương trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'be.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
